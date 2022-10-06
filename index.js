d406509073c95e02fd0aa57e76b6f99be3f31d67
smurtee <muratees@yahoo.com>
2022-10-05 21:28:00 -0500

    updated schema and  VIEW-roles/employees; addRole broken

--- index.js
+++ index.js
@@ -1,50 +1,163 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 let inquirer = require("inquirer")
 
-let connection
+// let connection
 
-initialize()
-main();
+// initialize()
 
 
-async function initialize(){
-    connection = await mysql.createConnection({host:'localhost', user: 'root', database: ''})
+// async function initialize(){
+let connection = mysql.createConnection({ host: 'localhost', user: 'root', database: 'employeeRecords_db', password: "" })
 // make sure to specify the workbench database**
-}
+// }
+connection.connect(function () {
+    main();
 
+})
 
-async function main() {
+function main() {
     // get the client
     // create the connection
-    const responseObject = await inquirer.prompt([ {
+    const responseObject = inquirer.prompt([{
         type: 'list',
-        name: 'first_name',
+        name: "prompt",
         message: "What would you like to do?",
         choices: ["View all departments", "View all roles",
-        "View all employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee role",
-        "Exit"]
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Exit"]
 
-      },
-      {
-        type: 'input',
-        name: 'last_name',
-        message: "What's your last name",
-        default() {
-          return 'Doe';
-        },
-      }])
+    },
+    ])
+        .then(response => {
+            if (response.prompt === "View all departments") {
+                viewDepartments()
+            }
+            else if (response.prompt === "View all roles") {
+                viewRoles()
+            }
+            else if (response.prompt === "View all employees") {
+                viewEmployees()
+            }
+            else if (response.prompt === "Add a role") {
+                addRole()
+            }
+            else if (response.prompt === "Add an employee") {
+                addEmployee()
+            }
+        })
 
-      console.log(responseObject)
+    console.log(responseObject)
 
 
     // query database
-    const [rows] = await connection.execute(`SELECT * FROM employees where firstname = ?`,[responseObject.first_name] );
-    console.table(rows);
+    // const [rows] = await connection.execute(`SELECT * FROM employees where first_name = ?`,[responseObject.first_name] );
+    // console.table(rows);
 
+}
 
+function viewDepartments() {
+    connection.query("SELECT * FROM department", (err, res) => {
+        if (err) throw err
+        console.table(res)
+        main()
+    })
+}
+function viewRoles() {
+    connection.query(`SELECT roles.id, roles.title, roles.salary, department.name AS department
+    FROM roles
+    INNER JOIN department ON roles.department_id = department.id;`
+, (err, res) => {
+        if (err) throw err
+        console.table(res)
+        main()
+    })
+}
+function viewEmployees() {
+    connection.query(`SELECT employees.id, 
+    employees.first_name, 
+    employees.last_name, 
+    roles.title, 
+    department.name AS department,
+    roles.salary, 
+    CONCAT (manager.first_name, " ", manager.last_name) AS manager
+    FROM employees
+    LEFT JOIN roles ON employees.role_id = roles.id
+    LEFT JOIN department ON roles.department_id = department.id
+    LEFT JOIN employees manager ON employees.manager_id = manager.id;`
+, (err, res) => {
+        if (err) throw err
+        console.table(res)
+        main()
+    })
+}
+function addRole() {
+    connection.query("SELECT * FROM roles", (err, res) => {
+        inquirer.prompt([
 
-  }
+            {
+                type: "input",
+                name: "title",
+                message: "What is your title?",
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "What is your salary?",
+            },
+            {
+                type: "list",
+                name: "department_id",
+                message: "which department?",
+                choices: res.map(department => department.name),
+            },
+        ]).then(data => {
+            const chosenDept = res.find(department_id => department_id.name === data.departments)
+            connection.query("INSERT INTO roles SET ?", {
+                title: data.title,
+                salary: data.salary,
+                department_id: chosenDept.id,
+            })
+            main()
+        })
+    })
+}
+// function addEmployee() {
+//     connection.query("SELECT * FROM department", (err, res) => {
+//         inquirer.prompt([
+
+//             {
+//                 type: "input",
+//                 name: "first_name",
+//                 message: "What is the employee's first name?",
+//             },
+//             {
+//                 type: "input",
+//                 name: "last_name",
+//                 message: "What is the employee's last name?",
+//             },
+//             {
+//                 type: "input",
+//                 name: "role_name",
+//                 message: "What is the employee's role?",
+//             },
+//             {
+//                 type: "list",
+//                 name: "manager_name",
+//                 message: "Who is the employee's manager?",
+//                 choices: res.map(department => department.name),
+//             },
+//         ]).then(data => {
+//             const chosenDept = res.find(department => department.name === data.departments)
+//             connection.query("INSERT INTO roles SET ?", {
+//                 title: data.title,
+//                 salary: data.salary,
+//                 department_id: chosenDept.id,
+//             })
+//             main()
+//         })
+//     })
+// }
--- schema.sql
+++ schema.sql
@@ -12,7 +12,10 @@ CREATE TABLE roles (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL,
-department_id INT
+department_id INT,
+FOREIGN KEY (department_id)
+REFERENCES department(id)
+ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -20,7 +23,13 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
-manager_id INT
+manager_id INT,
+FOREIGN KEY (role_id)
+REFERENCES roles(id)
+ON DELETE CASCADE,
+FOREIGN KEY (manager_id)
+REFERENCES employees(id)
+ON DELETE SET NULL
 );
 
 INSERT INTO department (name)
@@ -40,10 +49,11 @@ VALUES ("CEO", 200000, 1),
        
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Meyton", "Panning", 1, null),
-	   ("Bom", "Trady", 2, null),
-       ("Wurt", "Karner", 3, 1),
-       ("Rony", "Tomo", 3, null),
-       ("Pak", "Drescott", 3, null);
+	   ("Bom", "Trady", 2, 1),
+       ("Wurt", "Karner", 3, 2),
+       ("Rony", "Tomo", 3, 2),
+       ("Pak", "Drescott", 5, 3),
+       ("Raron", "Aodgers", 4, 3);
        
 SELECT * FROM employeeRecords_db.department;
 SELECT * FROM employeeRecords_db.roles;
